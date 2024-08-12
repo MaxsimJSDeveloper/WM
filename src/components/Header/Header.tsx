f57ff2735cab67d9c95c6b08ecf8fe018690b376
Holovko Maksym <golovkomaksim852@gmail.com>
2024-08-12 20:22:12 +0300

    header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { Modal, Box, IconButton } from "@mui/material";
+import { RxHamburgerMenu } from "react-icons/rx";
+
+import css from "./Header.module.css";
+import CloseIcon from "@mui/icons-material/Close";
+import Navigation from "../Navigation/Navigation";
+
+const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <header className={css.head}>
+      <RxHamburgerMenu onClick={handleOpen} />
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-navigation"
+        aria-describedby="modal-navigation-description"
+      >
+        <Box
+          onClick={handleClose}
+          sx={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100vw",
+            height: "100vh",
+            bgcolor: "rgba(0, 0, 0, 0.5)",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Box
+            sx={{
+              position: "relative",
+              width: "70%",
+              maxWidth: "500px",
+              bgcolor: "background.paper",
+              p: 4,
+              borderRadius: 2,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <IconButton
+              onClick={handleClose}
+              sx={{
+                position: "absolute",
+                top: 8,
+                right: 8,
+              }}
+            >
+              <CloseIcon />
+            </IconButton>
+            <Navigation onLinkClick={handleClose} />
+          </Box>
+        </Box>
+      </Modal>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,31 @@
+import css from "./Navigation.module.css";
+
+interface NavigationProps {
+  onLinkClick: () => void;
+}
+
+const Navigation = ({ onLinkClick }: NavigationProps) => {
+  return (
+    <div>
+      <ul>
+        <li className={css.link}>
+          <a href="#" onClick={onLinkClick}>
+            Послуги
+          </a>
+        </li>
+        <li className={css.link}>
+          <a href="#" onClick={onLinkClick}>
+            Посилання
+          </a>
+        </li>
+        <li className={css.link}>
+          <a href="#" onClick={onLinkClick}>
+            Підтримати проект
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navigation;
